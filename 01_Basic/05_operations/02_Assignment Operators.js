1323022a42749dbd1da7d9314fe6bd6d71a3c514
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-07-01 10:48:46 +0000

    Assignment Operators

---
+++ 01_Basic/05_operations/02_Assignment Operators.js
@@ -0,0 +1,43 @@
+
+//02 _ ASSIGNMENT OPERATORS CHEAT SHEET
+
+
+// 1. Basic Assignment (=) -> Value store karta hai
+let x = 10; 
+console.log(x); // Output: 10
+
+
+// 2. Add and Assign (+=) -> Purani value mein add karke update karta hai
+let a = 5;
+a += 3;         // Iska matlab hai: a = a + 3
+console.log(a); // Output: 8
+
+
+// 3. Subtract and Assign (-=)
+let b = 10;
+b -= 4;         // Iska matlab hai: b = b - 4
+console.log(b); // Output: 6
+
+
+// 4. Multiply and Assign (*=)
+let c = 5;
+c *= 2;         // Iska matlab hai: c = c * 2
+console.log(c); // Output: 10
+
+
+// 5. Divide and Assign (/=)
+let d = 20;
+d /= 4;         // Iska matlab hai: d = d / 4
+console.log(d); // Output: 5
+
+
+// 6. Modulus and Assign (%=) -> Remainder nikal kar update karta hai
+let e = 11;
+e %= 3;         // Iska matlab hai: e = e % 3 (3*3=9, bacha 2)
+console.log(e); // Output: 2
+
+
+// 7. Exponentiation and Assign (**=) -> Power nikal kar update karta hai
+let f = 3;
+f **= 3;        // Iska matlab hai: f = f ** 3 (3 * 3 * 3)
+console.log(f); // Output: 27
